4a518156ecacd50a941d4fdafae78ab73fe1bd6f
Alvaro2005 <78221033+Alvaro2005@users.noreply.github.com>
2021-07-11 13:31:40 -0400

    los efectos ya puestos y Probados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+window.addEventListener("scroll", () => {
+
+    function fade(direccion) {
+
+        let imagen = document.querySelectorAll(".fade_" + direccion);
+
+        for (let i = 0; i < imagen.length; i++) {
+
+            let altura = window.innerHeight / 1.2;
+
+            let distancia = imagen[i].getBoundingClientRect().top;
+
+            console.log(altura);
+
+            console.log(distancia);
+
+            imagen[i].classList.add("transfort_" + direccion);
+
+            if (distancia <= altura) {
+
+                imagen[i].classList.add("aparece");
+
+            } else {
+
+                imagen[i].classList.remove("aparece");
+
+            }
+        }
+    };
+
+    fade("up");
+    fade("left");
+    fade("right");
+    fade("down");
+});
+
 ((d) => {
     const $btnMenu = d.querySelector(".menu-btn");
     const $menu = d.querySelector(".menu");
